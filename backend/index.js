05601c076f204e0b718074a02aa6d4b26746e0ec
Aapo Humina <rageidos@gmail.com>
2025-01-17 08:50:49 +0200

    cleaned up index.js and added some tests

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,8 @@
-const express = require('express')
+const express = require('express');
 const app = express();
-const router = express.Router();
-const sqlite3 = require('sqlite3').verbose();
+const worddbRoutes = require('./routes/worddbRoutes');
+const cors = require('cors');
 
-const db = new sqlite3.Database(':memory:');
 //const port = 3000
 const port = process.env.PORT || 3000;
 
@@ -11,42 +10,9 @@ const path = require("path");
 app.use(express.static(path.join(__dirname, "public")));
 app.use(express.json());
 
-app.use("/api/worddb", router);
+app.use(cors());
 
-
-// Create a table and insert data
-db.serialize(() => {
-    // Create the table
-    db.run('CREATE TABLE IF NOT EXISTS worddb (id INTEGER PRIMARY KEY, finnish_word VARCHAR, english_word VARCHAR)', (err) => {
-        if (err) {
-            console.error('Error creating table:', err.message);
-            return;
-        }
-
-        // Insert data after table creation
-        db.run('INSERT INTO worddb (finnish_word, english_word) VALUES (?, ?)', ["koira", "dog"]);
-        db.run('INSERT INTO worddb (finnish_word, english_word) VALUES (?, ?)', ["kissa", "cat"]);
-        db.run('INSERT INTO worddb (finnish_word, english_word) VALUES (?, ?)', ["koti", "home"]);
-        db.run('INSERT INTO worddb (finnish_word, english_word) VALUES (?, ?)', ["joulu", "christmas"]);
-    });
-});
-
-// Define the route to get all locations
-const getAllWords = (req, res) => {
-    // Query the data
-    db.all('SELECT * FROM worddb ORDER BY id ASC', [], (err, rows) => {
-        if (err) {
-            return res.status(500).json({ error: err.message });
-        }
-        res.json(rows); // Send the rows as a JSON response
-    });
-};
-
-router.get('/', getAllWords);
-
-app.get('/', (req, res) => {
-    res.send('Hello World!')
-})
+app.use("/api/worddb", worddbRoutes);
 
 const server = app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
