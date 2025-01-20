bc52b261e96009a21e739cf2d0ced9666ab59cd6
Aapo Humina <rageidos@gmail.com>
2025-01-20 03:26:04 +0200

    fixed handleupdate

--- frontend/src/TeacherPage.jsx
+++ frontend/src/TeacherPage.jsx
@@ -20,7 +20,7 @@ const TeacherPage = () => {
     const handleUpdateWord = async () => {
       try {
         // Sending PUT request to update the word in the backend
-        const response = await fetch(`http://localhost:3000/api/worddb/${editWord}`, {
+        const response = await fetch(`http://localhost:3000/api/worddb/${editWord.id}`, {
           method: 'PUT',
           headers: {
             'Content-Type': 'application/json',
